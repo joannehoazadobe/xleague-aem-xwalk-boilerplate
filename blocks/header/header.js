cc499ec1cf6b8e13d1f9bb02b2402fe50fcbad3c
joannehoazadobe <joaho@adobe.com>
2025-08-26 11:00:53 -0700

    add debug statement to header js.

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -111,6 +111,7 @@ export default async function decorate(block) {
   // load nav as fragment
   const navMeta = getMetadata('nav');
   const navPath = navMeta ? new URL(navMeta, window.location).pathname : '/nav';
+  console.log('navPath', navPath);
   const fragment = await loadFragment(navPath);
 
   // decorate nav DOM
